d6a8ebe30c988f594be0bb24b3cf4b3e4e20d4df
Norton_YR_Li <nortonlyr@gmail.com>
2020-04-21 00:14:35 -0400

    In progress

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -1,5 +1,29 @@
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
+import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Integer, String, Column, Sequence
+
+mysqlkey = os.environ('mysql_key')
+Base = declarative_base()
+get_session = sessionmaker()
+
+
+
+class Transaction(Base):
+    __tablename__ = 'transaction'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    custid = Column(Integer)
+    type = Column(String(250), nullable=False)
+    date = Column(Integer)
+    amt = Column(Integer)
+
+
+
 
 class XactionConsumer:
     def __init__(self):
@@ -10,14 +34,19 @@ class XactionConsumer:
         ## These are two python dictionarys
         # Ledger is the one where all the transaction get posted
         self.ledger = {}
-        # custBalances is the one where the current blance of each customer
+        # custBalances is the one where the current balance of each customer
         # account is kept.
         self.custBalances = {}
         # THE PROBLEM is every time we re-run the Consumer, ALL our customer
         # data gets lost!
         # add a way to connect to your database here.
+        conn = create_engine("mysql+pymysql://root:" + mysqlkey + "@localhost:3306/zipbank")
+        # df = pd.read_csv(data_storage_path + 'nyc_park_data2.csv', delimiter=',')
+        # df.to_sql(name='transaction', con=conn, schema='zipbank', if_exists='replace')
 
-        #Go back to the readme.
+
+
+
 
     def handleMessages(self):
         for message in self.consumer:
