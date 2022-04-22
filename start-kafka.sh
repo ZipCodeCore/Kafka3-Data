3ca941823157c43678bb9231e6d3013e8942fa5e
Kristofer <kris@youngers.org>
2022-04-22 16:22:34 -0400

    sync with kris changes

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-kafka-server-start /usr/local/etc/kafka/server.properties
+MACHARCH=`uname -m`
+MACM1="arm64"
+
+if [[ "$MACHARCH" == "$MACM1" ]]; then
+   /opt/homebrew/opt/kafka/bin/kafka-server-start /opt/homebrew/etc/kafka/server.properties
+else
+   kafka-server-start /usr/local/etc/kafka/server.properties
+fi
--- start-zookeeper.sh
+++ start-zookeeper.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties
+MACHARCH=`uname -m`
+MACM1="arm64"
+
+if [[ "$MACHARCH" == "$MACM1" ]]; then
+   /opt/homebrew/opt/kafka/bin/zookeeper-server-start /opt/homebrew/etc/kafka/zookeeper.properties
+else
+   zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties
+fi
